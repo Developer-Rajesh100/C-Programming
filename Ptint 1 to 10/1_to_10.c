6febeabe8894daf803d2a5d1cad1c55abeb7f76c
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-09-01 16:14:14 +0530

    1 to 10 Numbers Print

---
+++ Ptint 1 to 10/1_to_10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("1 to 10 Numbers is: ");
+    for (number = 1; number <= 10; number++)
+    {
+        printf("%d \t", number);
+    }
+}
