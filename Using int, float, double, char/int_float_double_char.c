8a21497ca4a5de3b64019f21f9376cb7334e3a97
Developer-Rajesh100 <rajeshhabra100@gmail.com>
2023-08-29 23:34:15 +0530

    Using int, float, double, char updated

--- Using int, float, double, char/int_float_double_char.c
+++ Using int, float, double, char/int_float_double_char.c
@@ -2,16 +2,16 @@
 
 #include <stdio.h>
 
-int main() 
+int main()
 {
-  int i = 10;
-  float f = 15.7;
-  double d = 7.921544;
-  char c = 'R';
+    int i = 10;
+    float f = 15.7;
+    double d = 7.921544;
+    char c = 'M';
 
-  printf("i = %d, \n f = %f, \n d = %lf, \n c = %c", i, f, d, c);
-  
-  printf("\n\n\n i = %d, \n f = %.1f, \n d = %.4lf, \n c = %c", i, f, d, c);
-  
-  return 0;
+    printf("i = %d, \n f = %f, \n d = %lf, \n c = %c", i, f, d, c);
+
+    printf("\n\n\n i = %d, \n f = %.1f, \n d = %.4lf, \n c = %c", i, f, d, c);
+
+    return 0;
 }
