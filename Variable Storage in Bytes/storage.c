6d611e700285e1d772508268e997c6331393141e
Developer-Rajesh100 <rajeshhabra100@gmail.com>
2023-08-30 11:31:19 +0530

    Variable Storage in Bytes

---
+++ Variable Storage in Bytes/storage.c
@@ -0,0 +1,15 @@
+// Variable Storage
+
+#include <stdio.h>>
+
+int main()
+{
+    int a;
+    float f;
+    double d;
+    char c;
+
+    printf("int : %d, \nfloat : %d, \ndouble : %d, \nchar : %d,", sizeof(a), sizeof(f), sizeof(d), sizeof(c));
+
+    return 0;
+}
