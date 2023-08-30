19dcc4c12a73051ba16621d14a924c338bf7afe8
Developer-Rajesh100 <rajeshhabra100@gmail.com>
2023-08-30 11:42:16 +0530

    Ascii Value to Character

---
+++ Anusul Islam C/3) Ascii Value to Character/ascii_to_character.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter any Number : ");
+    scanf("%d", &number);
+
+    printf("The ASCII Character is : %c", number);
+
+    return 0;
+}
